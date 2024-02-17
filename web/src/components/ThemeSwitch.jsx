e45a1e10fe315a1d73b5d05b91df53e662478c5d
Doniyor <doniyori@buffalo.edu>
2024-02-17 14:34:08 -0500

    Added Routes and Button

--- web/src/components/ThemeSwitch.jsx
+++ web/src/components/ThemeSwitch.jsx
@@ -14,15 +14,16 @@ export default function ThemeSwitch() {
 // This !mounted check is used to prevent the theme switcher from rendering on the server.
 
   if (!mounted) return (
-    <Image
-      src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-      width={16} //Keep the width and height the same as the icons 
-      height={16}
-      sizes="36x36"
-      alt="Loading Light/Dark Toggle"
-      priority={false}
-      title="Loading Light/Dark Toggle"
-    />
+    // <Image
+    //   src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+    //   width={16} //Keep the width and height the same as the icons 
+    //   height={16}
+    //   sizes="36x36"
+    //   alt="Loading Light/Dark Toggle"
+    //   priority={false}
+    //   title="Loading Light/Dark Toggle"
+    // />
+    null
   )
 
   if (resolvedTheme === 'dark') {
