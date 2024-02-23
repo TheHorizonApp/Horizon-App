148a8902882f24570799b7da4f481726976b76ab
Doniyor <doniyori@buffalo.edu>
2024-02-22 19:43:15 -0500

    Navbar pinned when when in dashbar

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,37 +1,50 @@
 "use client";
 import React from "react";
-import { FaHome, FaSearch, FaCalendarAlt, FaFileAlt, FaUser, FaCog } from "react-icons/fa";
+import {
+  FaHome,
+  FaSearch,
+  FaCalendarAlt,
+  FaFileAlt,
+  FaUser,
+  FaCog,
+} from "react-icons/fa";
 import { usePathname } from "next/navigation";
 
 function Navbar({ darkMode }) {
   const pathname = usePathname();
-  const shouldHideNavbar =
-    pathname === "/" || pathname === "/login" || pathname === "/register";
+  const shouldHideNavbar = pathname === "/" || pathname === "/login" || pathname === "/register";
+  const pin = pathname === "/dashboard";
 
   if (shouldHideNavbar) {
     return null;
   }
+
+  const navbarClasses = `navbar-hover bg-white dark:bg-black ${pin ? "navbar-pinned" : ""}`;
+
   return (
-    <div className={`navbar-hover bg-white dark:bg-black `}>
+    <div className={navbarClasses}>
       <div className="navbar-icons-container">
         <div className="flex flex-col items-center space-y-10 my-20">
-          <NavbarIcon icon={<FaHome size="24" />} />
-          <NavbarIcon icon={<FaSearch size="24" />} />
-          <NavbarIcon icon={<FaCalendarAlt size="24" />} />
-          <NavbarIcon icon={<FaFileAlt size="24" />} />
-          <NavbarIcon icon={<FaUser size="24" />} />
+          {/* Pass `pin` to each NavbarIcon component */}
+          <NavbarIcon icon={<FaHome size="24" />} visible={pin} />
+          <NavbarIcon icon={<FaSearch size="24" />} visible={pin} />
+          <NavbarIcon icon={<FaCalendarAlt size="24" />} visible={pin} />
+          <NavbarIcon icon={<FaFileAlt size="24" />} visible={pin} />
+          <NavbarIcon icon={<FaUser size="24" />} visible={pin} />
         </div>
 
         <div className="flex items-center justify-center mb-10">
-          <NavbarIcon icon={<FaCog size="24" />} darkMode={darkMode} />
+          <NavbarIcon icon={<FaCog size="24" />} visible={pin} darkMode={darkMode} />
         </div>
       </div>
     </div>
   );
 }
 
-function NavbarIcon({ icon, darkMode }) {
-  return <div className={`navbar-icon dark:text-white text-black`}>{icon}</div>;
+function NavbarIcon({ icon, visible, darkMode }) {
+  const iconClasses = `navbar-icon dark:text-white text-black ${visible ? "navbar-icon-visible" : ""}`;
+
+  return <div className={iconClasses}>{icon}</div>;
 }
 
 export default Navbar;
