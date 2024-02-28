aade72fe6bce7ca6220d8cdd2c71d40e90b3c154
DoniyorI <doniyor.ismatilloev@gmail.com>
2024-02-28 12:43:23 -0500

    Dashboard Layout

--- web/src/app/dashboard/page.jsx
+++ web/src/app/dashboard/page.jsx
@@ -1,12 +1,42 @@
-import React from "react";
 import ToDo from "@/components/ToDo";
 
 const page = () => {
+  const name = "John Doe";
+  const getGreetingAndColor = () => {
+    const hour = new Date().getHours();
+    if (hour < 6) return ["Good Night", "text-indigo-900"]; // Late night to early morning
+    if (hour < 12) return ["Good Morning", "text-blue-400"]; // Morning
+    if (hour < 18) return ["Good Afternoon", "text-yellow-300"]; // Afternoon
+    if (hour < 20) return ["Good Evening", "text-orange-500"]; // Evening
+    return ["Good Night", "text-indigo-900"]; // Night
+  };
+
+  const [greeting, colorClass] = getGreetingAndColor();
+
   return (
     <div className="flex h-screen">
-      <div className="flex-grow">
+      <div className="flex-grow m-4">
         {/* Main content goes here */}
-        Main content
+        <h1 className={`text-3xl font-medium ${colorClass}`}>
+          {greeting}{" "}
+          <span className="text-indigo-500 drop-shadow-lg font-semibold">
+            {name}
+          </span>
+        </h1>
+        {/* TODO: fix where that  */}
+        <div className="flex flex-col space-y-4 p-4 h-[93vh]"> 
+          <div className="border border-gray-500 rounded-xl p-4 h-full flex-grow">
+            Calendar
+          </div>
+          <div className="flex space-x-4 h-2/5">
+            <div className="border border-gray-500 rounded-xl p-4 flex-grow w-1/2">
+              Quick Notes
+            </div>
+            <div className="border border-gray-500 rounded-xl p-4 flex-grow w-1/2">
+              Deadlines
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-1/5 border-l border-slate-700">
         <div className="m-6 mt-8">
--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,33 +1,69 @@
-"use client"
+"use client";
 import React from "react";
-import { FaHome, FaSearch, FaCalendarAlt, FaFileAlt, FaUser, FaCog } from "react-icons/fa";
+import {
+  FaHome,
+  FaSearch,
+  FaCalendarAlt,
+  FaFileAlt,
+  FaUser,
+  FaCog,
+} from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
 
 function Navbar() {
   const pathname = usePathname();
-  const shouldHideNavbar = pathname === "/" || pathname === "/login" || pathname === "/register";
+  const shouldHideNavbar =
+    pathname === "/" || pathname === "/login" || pathname === "/register";
   const pin = pathname === "/dashboard";
 
   if (shouldHideNavbar) {
     return null;
   }
 
-  const navbarClasses = `navbar-hover bg-white dark:bg-black ${pin ? "navbar-pinned" : ""}`;
+  const navbarClasses = `navbar-hover bg-white dark:bg-black ${
+    pin ? "navbar-pinned" : ""
+  }`;
 
   return (
     <div className={navbarClasses}>
       <div className="navbar-icons-container">
-        <div className="flex flex-col items-center space-y-10 my-20">
-          <NavbarIcon icon={<FaHome size="24" />} visible={pin} href="/dashboard" />
-          <NavbarIcon icon={<FaSearch size="24" />} visible={pin} href="/search" />
-          <NavbarIcon icon={<FaCalendarAlt size="24" />} visible={pin} href="/calendar" />
-          <NavbarIcon icon={<FaFileAlt size="24" />} visible={pin} href="/notes" />
-          <NavbarIcon icon={<FaUser size="24" />} visible={pin} href="/profile" />
+        <h1 className="text-center py-4 font-semibold">Horizon</h1>
+        <div className="flex flex-col items-center space-y-10 my-10">
+          <NavbarIcon
+            icon={<FaHome size="24" />}
+            visible={pin}
+            href="/dashboard"
+          />
+          <NavbarIcon
+            icon={<FaSearch size="24" />}
+            visible={pin}
+            href="/search"
+          />
+          <NavbarIcon
+            icon={<FaCalendarAlt size="24" />}
+            visible={pin}
+            href="/calendar"
+          />
+          <NavbarIcon
+            icon={<FaFileAlt size="24" />}
+            visible={pin}
+            href="/notes"
+          />
+          <NavbarIcon
+            icon={<FaUser size="24" />}
+            visible={pin}
+            href="/profile"
+          />
+          <ThemeSwitch />
         </div>
-
         <div className="flex items-center justify-center mb-10">
-          <NavbarIcon icon={<FaCog size="24" />} visible={pin} href="/settings" />
+          <NavbarIcon
+            icon={<FaCog size="24" />}
+            visible={pin}
+            href="/settings"
+          />
         </div>
       </div>
     </div>
@@ -35,9 +71,14 @@ function Navbar() {
 }
 
 function NavbarIcon({ icon, visible, href }) {
-  const iconClasses = `navbar-icon dark:text-white text-black ${visible ? "navbar-icon-visible" : ""}`;
-  return <Link href={href}className={iconClasses}>{icon}</Link>
-  ;
+  const iconClasses = `navbar-icon dark:text-white text-black ${
+    visible ? "navbar-icon-visible" : ""
+  }`;
+  return (
+    <Link href={href} className={iconClasses}>
+      {icon}
+    </Link>
+  );
 }
 
 export default Navbar;
