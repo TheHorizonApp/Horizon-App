764458c66779e77ba9dfc44ac4c53aeee6636f1e
ZodinTC <zodin.thanga9@gmail.com>
2024-03-11 16:51:02 -0400

    hides creds

--- backend/.aws/aws_prof_imgs/aws_prof_imgs.go
+++ backend/.aws/aws_prof_imgs/aws_prof_imgs.go
@@ -3,9 +3,11 @@ package aws_prof_imgs
 import (
 	"bytes"
 	"context"
-
+	"os"
+	"encoding/json"
 	"net/http"
-
+	"fmt"
+	"path/filepath"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -17,13 +19,47 @@ type S3Uploader struct {
 	s3Client *s3.S3
 	bucket   string
 }
+type Credentials struct {
+	AWS_REGION 				string `json:"AWS_REGION"`
+	AWS_ACCESS_KEY_ID   	string `json:"aws_access_key_id"`
+	AWS_SECRET_ACCESS_KEY 	string `json:"aws_secret_access_key"`
 
+}
+
+func loadCredentials() (*Credentials, error) {
+
+	absPath, err := filepath.Abs("../creds.json")
+    if err != nil {
+        return nil, fmt.Errorf("failed to get absolute path for creds.json: %v", err)
+    }
+	file, err := os.Open(absPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open creds.json: %v", err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	creds := &Credentials{}
+	err = decoder.Decode(creds)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode creds.json: %v", err)
+	}
+	println(creds.AWS_SECRET_ACCESS_KEY)
+	println(creds.AWS_ACCESS_KEY_ID)
+	println(creds.AWS_REGION)
+	return creds, nil
+}
 // NewS3Uploader creates a new S3Uploader instance.
 func NewS3Uploader(bucket string) (*S3Uploader, error) {
-	// Initialize an AWS session and S3 client
+
+	creds, err := loadCredentials()
+	if err != nil {
+		return nil, err
+	}
+
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
-		Credentials: credentials.NewStaticCredentials("AKIA3FLD4G6CQNG6EKGB", "6AGEYyOLBNcfCVjcJhg3to3sPc6GTNGWMDKWt4d+", " "),
+		Region: aws.String(creds.AWS_REGION),
+		Credentials: credentials.NewStaticCredentials(creds.AWS_ACCESS_KEY_ID, creds.AWS_SECRET_ACCESS_KEY, " "),
 	})
 	if err != nil {
 		return nil, err
